70443471c4a416b75176d53a18593727169a0f25
Abhishek Singh <Abhishek.topa@gmail.com>
2020-05-24 12:18:46 +0530

    Adding Branch  jaba file

---
+++ src/JavaProgrammes/Branching_GIT.java
@@ -0,0 +1,10 @@
+package JavaProgrammes;
+
+public class Branching_GIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Branching in GiT");
+	}
+
+}
--- src/JavaProgrammes/TestGit.java
+++ src/JavaProgrammes/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Git hub ");
-
+		System.out.println("Added few more lines");
 	}
 
 }
