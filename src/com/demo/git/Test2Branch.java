7dbc9112899bbe1ff570512c1df13b9419c72379
Abhishek Singh <Abhishek.topa@gmail.com>
2020-05-24 19:51:47 +0530

    Adding one more line of code in Test2Branch

--- src/com/demo/git/Test2Branch.java
+++ src/com/demo/git/Test2Branch.java
@@ -4,6 +4,8 @@ public class Test2Branch {
 
 	public static void main(String[] args) {
 System.out.println("Adding test 2 Branch");
+System.out.println("Adding one more test to Test2 Branch");
+
 	}
 
 }
